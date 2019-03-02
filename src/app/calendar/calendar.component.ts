ff77a2dc0b6794d32f01d1facfc7bbf9f8b77ac5
AlexDegterov <degterov@proektant.by>
2019-03-02 20:49:19 +0300

    before transaction date between component

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,74 +7,103 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CalendarComponent implements OnInit {
-  private arrMonthRu: string[];
-  private now: any;
-  private yearNow: number;
-  private monthNow: number;
-  private dateNow: number;
-  private firstDayWeekInMonth: number; // день  недели с которого начинается месяц
-  private daysInMonth: number;    // дней в месяце
+  public arrMonthRu: string[];    // массив русских месяцев
+  private nowTime: any;           // текущее время (new Date())
+  private year: number;           // номер года в календаре
+  private month: number;          // номер месяца в календаре
+  public day: number;             // номер дня в календаре
+  private firstDayWeekInMonth: number; // день  недели с которого начинается выбранный месяц
+  public daysInMonth: number;     // всего дней в выбранном месяце
 
-  private arrDate = [];   // Массив всех дней месяца
+  public yearNow: number;        // номер текущего года
+  public monthNow: number;       // номер текущего месяца
+  public dayNow: number;         // номер текущего дня
+
+  public arrDate = [];            // массив всех дней месяца для вывода на странице
   private i: number;
-  private tt: string;
 
-  weekEnd = [5, 6, 12, 13, 19, 20, 26, 27, 33, 34, 40, 41];   // Массив выходных
+  private weekEnd = [5, 6, 12, 13, 19, 20, 26, 27, 33, 34, 40, 41];   // массив выходных
 
   // Получение массива дней в месяце
   arrD() {
     this.arrDate = [];
-    this.firstDayWeekInMonth = new Date(this.yearNow, this.monthNow, 1, 15).getDay();
+    this.firstDayWeekInMonth = new Date(this.year, this.month, 1, 15).getDay();
     if (this.firstDayWeekInMonth === 0) { this.firstDayWeekInMonth = 7; }
-    this.daysInMonth = 32 - new Date(this.yearNow, this.monthNow, 32).getDate();
+    this.daysInMonth = 32 - new Date(this.year, this.month, 32).getDate();
 
     // Массив всех дней, анализ выходных, дня начала месяца
 // tslint:disable-next-line: max-line-length
     for (this.i = 0; this.i < ((this.daysInMonth >= 30 && this.firstDayWeekInMonth >= 7) || (this.daysInMonth >= 31 && this.firstDayWeekInMonth >= 6) ? 42 : 35); this.i++) {
 // tslint:disable-next-line: max-line-length
-      this.arrDate.push ({day: this.i, dayW: this.i + 2 - this.firstDayWeekInMonth, holiday: (this.weekEnd.indexOf(this.i) < 0) ? false : true});
+      this.arrDate.push ({day: this.i, dayW: this.i + 2 - this.firstDayWeekInMonth, holiday: (this.weekEnd.indexOf(this.i) < 0) ? false : true, today: ((this.i + 2 - this.firstDayWeekInMonth === this.day) && this.yearNow === this.year && this.monthNow === this.month) ? true : false});
     }
     return this.arrDate;
   }
 
   lessYear() {
-    this.yearNow--;
+    this.year--;
     this.arrD();
   }
 
   lessMonth() {
-    if (this.monthNow > 0) {
-      this.monthNow -= 1;
+    if (this.month > 0) {
+      this.month -= 1;
    } else {
-      this.monthNow = 11;
-      this.yearNow -= 1;
+      this.month = 11;
+      this.year -= 1;
    }
     this.arrD();
   }
 
   moreMonth() {
-    if (this.monthNow < 11) {
-      this.monthNow += 1;
+    if (this.month < 11) {
+      this.month += 1;
    } else {
-    this.monthNow = 0;
-    this.yearNow += 1;
+    this.month = 0;
+    this.year += 1;
    }
     this.arrD();
   }
 
   moreYear() {
-    this.yearNow++;
+    this.year++;
     this.arrD();
   }
   constructor() { }
 
   ngOnInit() {
     this.arrMonthRu = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-    this.now = new Date();
-    this.yearNow = this.now.getFullYear();
-    this.monthNow = this.now.getMonth();
-    this.dateNow = this.now.getDate();
+    this.nowTime = new Date();
+    this.year = this.nowTime.getFullYear();
+    this.month = this.nowTime.getMonth();
+    this.day = this.nowTime.getDate();
+
+    this.yearNow = this.year;         // сохраняем текущий год
+    this.monthNow = this.month;       // сохраняем текущий месяц
+    this.dayNow = this.day;           // сохраняем текущий день
 
     this.arrD();
+  }
+
+  selectDay(ev) {
+    let arrHighlightClick;
+
+    if (ev.target.dataset.date < 1) { return; }
+
+    this.arrHighlightClick = document.getElementsByClassName("highlightClick");
+    if (this.arrHighlightClick.length > 0) {
+      this.arrHighlightClick[0].classList.remove('highlightClick');
+    }
+      
+    this.day = null;
+    if(ev.target.innerText) {
+      if (ev.target.tagName !== 'DIV') {
+        this.day = ev.target.parentNode.dataset.date;
+        ev.target.parentNode.classList.add('highlightClick');
+      } else {
+        this.day = ev.target.dataset.date;
+        ev.target.classList.add('highlightClick');
+      }
+    }
   }
 }
